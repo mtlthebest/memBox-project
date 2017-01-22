e318bbbd0c6a81d5a05c2aa99e867558f0cc5c47
Matteo Laffranchi <mtl@libero.it>
2017-01-22 23:53:14 +0100

    Eseguiti esercizi con gdb

--- sandbox/Esercitazione 1/debug/findbug.c
+++ sandbox/Esercitazione 1/debug/findbug.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>		// inserito da MtL
 
 #define N 128
 int main()
 {
-    char *A;
-    int i = 0;
+    char *A = malloc(N * sizeof(char));	// modificato da MtL
+    int i = -1;			// modificato da MtL
     do {
 	A[++i] = !(i % 2);
     }
-    while (i < N);
+    while (i < N - 1);		// modificato da MtL
 
     printf("stampa ...\n");
     for (int i = 0; i < N; ++i)
 	printf("%d ", A[i]);
     printf("\nesco\n");
+    free(A);			// inserito da MtL
     return 0;
 }
