465f2f12a3b165f03fbc9ee55522a80362ed011a
Matteo Laffranchi <mtl@libero.it>
2017-01-29 23:41:25 +0100

    Concluso esercizio tokenizer

--- Esercitazioni di Laboratorio/Assegnamento 2/Esercitazione 1 (tokenizer)/tokenizer.c
+++ Esercitazioni di Laboratorio/Assegnamento 2/Esercitazione 1 (tokenizer)/tokenizer.c
@@ -18,53 +18,37 @@
 
 #include <stdio.h>
 #include <string.h>
+#include "tokenizer.h"
 
 int
 main (int argc, char *argv[])
 {
-
-  // determino la lunghezza totale della stringa fornita
-  int i;
-  int lunghezza = 0;
-  for (i = 1; i < argc; i++)
-    lunghezza += strlen (argv[i]);
-
-  printf ("Lunghezza: %d\n", lunghezza);
-
-  // costruisco la stringa intera unendo gli elementi di argv
+  // nessuna stringa fornita
   if (argc <= 1)
-    printf ("Numero di argomenti insufficiente.\n");
+    {
+      printf ("Numero di argomenti insufficiente.\n");
+      return 0;
+    }
+
+  // fornita piu' di una stringa
   else
     {
-      char delimitatore[] = " ";
-      char testo[lunghezza + 1];
-      for (i = 0; i < (lunghezza + 1); i++)
-	testo[i] = '\0';
-
-      // copio solo il primo argomento
-      strcat (testo, argv[1]);
-
-      // copio i rimanenti
-      for (i = 2; i < argc; i++)
-	{
-	  strcat (testo, delimitatore);
-	  strcat (testo, argv[i]);
-	}
-
-      //char testo[] = "Hello, Operating Systems World!";
-      char *token;
-
-      token = strtok (testo, delimitatore);
-      while (token != NULL)
-	{
-	  printf ("%s\n", token);
-	  token = strtok (NULL, delimitatore);
-	}
-    }				// end big if
-  return 0;
+      int i;
+      for (i = 1; i < argc; i++)
+	tokenizer (argv[i], stdout);
+      return 0;
+    }
 }
 
 void
 tokenizer (char *stringa, FILE * out)
 {
+  char delimitatore[] = " ";
+  char *token;
+  token = strtok (stringa, delimitatore);
+  while (token != NULL)
+    {
+      fprintf (out, "%s\n", token);
+      token = strtok (NULL, delimitatore);
+    }
 }
