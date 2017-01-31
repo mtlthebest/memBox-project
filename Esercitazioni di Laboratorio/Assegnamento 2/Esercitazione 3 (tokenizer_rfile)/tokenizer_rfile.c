695f2f7ec0cdc11b8f48cca47fe25ae72fac9ebf
Matteo Laffranchi <mtl@libero.it>
2017-01-31 23:29:15 +0100

    Concluso tokenizer_rfile

--- Esercitazioni di Laboratorio/Assegnamento 2/Esercitazione 3 (tokenizer_rfile)/tokenizer_rfile.c
+++ Esercitazioni di Laboratorio/Assegnamento 2/Esercitazione 3 (tokenizer_rfile)/tokenizer_rfile.c
@@ -14,31 +14,42 @@
  * in fondo al file.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #define LIMIT 1024
 
+// definizione struttura per i singoli token
+struct token_element
+{
+  struct token_element *precedente;
+  char *stringa_1K_token;
+  struct token_element *successivo;
+};
+
+// definizione struttura per le righe estratte
 struct elemento_lista_doppia
 {
   struct elemento_lista_doppia *precedente;
   char *stringa_1K;
+  int token_count;
+  struct token_element *token_list;
   struct elemento_lista_doppia *successivo;
-  int matricola;
 };
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tokenizer_rfile.h"
 
 int
 main (int argc, char *argv[])
 {
   if (argc <= 2)
     {
-      printf ("Errore: fornire almeno due argomenti.\n");
+      fprintf (stderr, "Errore: fornire almeno due argomenti.\n");
       return 0;
     }
   else if (argc >= 5)
     {
-      printf ("Errore: fornire al piu' tre argomenti.\n");
+      fprintf (stderr, "Errore: fornire al piu' tre argomenti.\n");
       return 0;
     }
   else
@@ -53,13 +64,35 @@ main (int argc, char *argv[])
 	}
       else
 	{
+	  // FASE 1: SEPARAZIONE DELLE RIGHE
 	  // il file di input esiste, procedo con l'esecuzione del programma
+	  printf ("File di input: \"%s\".\n", argv[1]);
+
+	  // apro il file di output
+	  FILE *testo_out;
+	  if (argc == 4 && (strcmp (argv[3], "a") == 0))
+	    {
+	      testo_out = fopen (argv[2], "a");
+	      printf
+		("File di output: \"%s\" (utilizzato in modalita' append).\n",
+		 argv[2]);
+
+	    }
+	  else
+	    {
+
+	      testo_out = fopen (argv[2], "w");
+	      printf
+		("File di output: \"%s\" (utilizzato in modalita' creazione/sovrascrittura).\n",
+		 argv[2]);
+	    }
 
 	  // creazione delle variabili necessarie sullo stack
-	  int parole = 0;
+	  int numero_righe = 0;
 	  char *temp = malloc ((LIMIT + 1) * sizeof (char));	// max 1K caratteri + '\0
 	  struct elemento_lista_doppia *base;
 	  struct elemento_lista_doppia *cur;
+	  int i;
 
 	  // gestisco il primo elemento della lista doppia
 	  char *feedback = NULL;
@@ -73,10 +106,7 @@ main (int argc, char *argv[])
 	      base->stringa_1K = malloc ((LIMIT + 1) * sizeof (char));
 	      strcpy (base->stringa_1K, temp);
 	      base->successivo = NULL;
-	      parole++;
-	      printf ("--> base content: %s\n", base->stringa_1K);	// debug
-	      printf
-		("Debug: elemento di base creato e popolato con successo.\n");
+	      numero_righe++;
 	    }
 
 	  // proseguo iterativamente con la creazione degli elementi
@@ -84,7 +114,6 @@ main (int argc, char *argv[])
 	  cur = base;		// imposto il cursore  
 	  while (fgets (temp, LIMIT + 1, testo_in) != NULL)
 	    {
-	      printf ("Debug: sono dentro il ciclo while.\n");
 	      // allocazione memoria per l'elemento successivo
 	      cur->successivo =
 		malloc (sizeof (struct elemento_lista_doppia));
@@ -96,36 +125,126 @@ main (int argc, char *argv[])
 	      strcpy (cur->successivo->stringa_1K, temp);
 	      cur->successivo->successivo = NULL;
 
-	      // debug
-	      printf ("--> cursor content: %s\n",
-		      cur->successivo->stringa_1K);
+	      // incremento del contatore degli elementi della lista
+	      numero_righe++;
 
 	      // definizione del nuovo cursore
 	      cur = cur->successivo;
-
-	      // incremento del contatore degli elementi della lista
-	      parole++;
 	    }
-	  printf ("Numero di parole: %d\n", parole);
 
-	  // operazioni finali
-	  // chiusura del file di input
+	  // FASE 2: ESECUZIONE FUNZIONE TOKENIZER PER OGNI RIGA
+	  // impostazione del cursore
+	  cur = base;
+	  for (i = 0; i < numero_righe; i++)
+	    {
+	      cur->token_list = malloc (sizeof (struct token_element));
+	      cur->token_list->precedente = NULL;
+	      cur->token_list->successivo = NULL;
+	      cur->token_count =
+		tokenizer_rfile (cur->stringa_1K, cur->token_list);
+	      cur = cur->successivo;
+	    }
+
+	  // FASE 3: SCRITTURA OUTPUT (SENSO INVERSO)
+	  // posizionamente cursore sull'ultimo elemento
+	  cur = base;
+	  for (i = 0; i < numero_righe - 1; i++)
+	    cur = cur->successivo;
+	  for (i = 0; i < numero_righe; i++)
+	    {
+	      reverse_write_tokens (cur->token_list, cur->token_count,
+				    testo_out);
+	      // printf ("debug: qui sono arrivato.\n");
+	      cur = cur->precedente;
+	    }
+
+	  // FASE 4: OPERAZIONI FINALI
+	  // chiusura dei file di input e output
 	  fclose (testo_in);
+	  fclose (testo_out);
 
 	  // libero la memoria dinamica assegnata alla lista doppia
 	  free (temp);
-	  while (0)
+	  cur = base;
+	  struct elemento_lista_doppia *prev = cur;
+
+	  for (i = 0; i < numero_righe; i++)
 	    {
-	      // libero iterativamente tutta la memoria assegnata agli elementi
-	      // free();
-	    }
-	  if (feedback != NULL)
-	    {
-	      free (base->stringa_1K);
-	      free (base);
+	      prev = cur;	// prev e' il nodo su cui lavoro
+	      cur = cur->successivo;	// metto cur al sicuro sull'elemento successivo
+	      prev->precedente = NULL;	// prev: inizio la cancellazione dell'item
+	      free (prev->stringa_1K);
+	      free_memory (prev->token_list, prev->token_count);	// chiamo una funzione apposita
+	      prev->successivo = NULL;
+	      free (prev);
 	    }
 
+	  // end main
 	  return 0;
 	}
+    }
+
+}
+
+int
+tokenizer_rfile (char *riga_testo, struct token_element *base)
+{
+  int count = 0;
+  struct token_element *cursore = base;
+  char delimitatore[] = " ";
+  char *token;
+  char **save_pointer = malloc (sizeof (char *));
+  char **save_pointer_bis = malloc (sizeof (char *));
+  token = strtok_r (riga_testo, delimitatore, save_pointer);
+  while (token != NULL)
+    {
+      if (count > 0)
+	{			// gestione del cursore
+	  cursore->successivo = malloc (sizeof (struct token_element));
+	  cursore->successivo->precedente = cursore;
+	  cursore->successivo->successivo = NULL;
+	  cursore = cursore->successivo;
+	}
+      cursore->stringa_1K_token = malloc ((LIMIT + 1) * sizeof (char));
+      strtok_r (token, "\n", save_pointer_bis);	// eliminina eventuali "\n"
+      strcpy (cursore->stringa_1K_token, token);
+      count++;
+      // printf ("debug >>> Token: %s\n", cursore->stringa_1K_token);
+      token = strtok_r (NULL, delimitatore, save_pointer);
+    }
+  free (save_pointer);
+  free (save_pointer_bis);
+  return count;
+}
+
+void
+reverse_write_tokens (struct token_element *item, int numero_token,
+		      FILE * output)
+{
+  struct token_element *cursore = item;
+  int x;
+  for (x = 0; x < numero_token - 1; x++)
+    cursore = cursore->successivo;
+  for (x = numero_token; x > 0; x--)
+    {
+      fprintf (output, "%s\n", cursore->stringa_1K_token);
+      cursore = cursore->precedente;
+    }
+}
+
+void
+free_memory (struct token_element *item, int numero_token)
+{
+  struct token_element *cur = item;
+  struct token_element *prev;
+  int j;
+  for (j = 0; j < numero_token; j++)
+    {
+      prev = cur;		// prev e' il nodo su cui lavoro
+      cur = cur->successivo;	// metto cur al sicuro sull'elemento successivo
+      prev->precedente = NULL;	// prev: inizio la cancellazione dell'item
+      free (prev->stringa_1K_token);
+      prev->successivo = NULL;
+      free (prev);
     }
 }
---
+++ Esercitazioni di Laboratorio/Assegnamento 2/Esercitazione 3 (tokenizer_rfile)/tokenizer_rfile.h
@@ -0,0 +1,3 @@
+int tokenizer_rfile (char *, struct token_element *);
+void reverse_write_tokens (struct token_element *, int, FILE *);
+void free_memory (struct token_element *, int);
